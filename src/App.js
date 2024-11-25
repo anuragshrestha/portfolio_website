3ac6cb022b921fbcbb101277c9d5b28225c77215
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-24 19:49:51 -0700

    added profile image and animation text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Headers from './headers/Headers';
+import Introduction from './introduction/Introduction';
 
 function App() {
   return (
     <div className="App">
      <Headers/>
+     <Introduction/>
     </div>
   );
 }
