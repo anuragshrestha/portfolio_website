a629ccc4b4f19dfee35cafa3509816c25650eda5
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-24 19:53:28 -0700

    added section for about, skills, etc

--- src/headers/Headers.jsx
+++ src/headers/Headers.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import './Headers.css'
-import  linkedin_logo from  '../assests/logos/linkedin.png'
-import github_logo from '../assests/logos/github.png'
-import instagram_logo from '../assests/logos/instagram.png'
+import React from "react";
+import "./Headers.css";
+import linkedin_logo from "../assests/logos/linkedin.png";
+import github_logo from "../assests/logos/github.png";
+import instagram_logo from "../assests/logos/instagram.png";
 
 const Headers = () => {
-
-    const navigateTo = (url) => {
-        window.open(url, '_blank')
-    }
+  const navigateTo = (url) => {
+    window.open(url, "_blank");
+  };
 
   return (
     <div className="headers">
-     <img src={linkedin_logo} alt=''onClick={() => navigateTo('https://www.linkedin.com/in/anuragshrestha19/')} />
-     <img src={github_logo} alt='' onClick={() => navigateTo('https://github.com/anuragshrestha')}/>
-     <img src={instagram_logo} alt='' onClick={() => navigateTo('https://www.instagram.com/_anurag_shrestha_/')} />
+      <div className="headers-left">
+        <h1>About</h1>
+        <h1>Skills</h1>
+        <h1>Experience</h1>
+        <h1>Projects</h1>
+      </div>
+      <img
+        src={linkedin_logo}
+        alt=""
+        onClick={() =>
+          navigateTo("https://www.linkedin.com/in/anuragshrestha19/")
+        }
+      />
+      <img
+        src={github_logo}
+        alt=""
+        onClick={() => navigateTo("https://github.com/anuragshrestha")}
+      />
+      <img
+        src={instagram_logo}
+        alt=""
+        onClick={() =>
+          navigateTo("https://www.instagram.com/_anurag_shrestha_/")
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Headers;
