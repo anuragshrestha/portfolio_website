44486fd1b6b096b4b62449679272477f402eeef6
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-25 23:25:23 -0700

    created a skills section with all the logos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Headers from './headers/Headers';
 import Introduction from './introduction/Introduction';
+import Skills from './skills/Skills';
 
 function App() {
   return (
     <div className="App">
      <Headers/>
      <Introduction/>
+     <Skills/>
     </div>
   );
 }
